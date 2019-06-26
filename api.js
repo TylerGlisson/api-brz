fc3aac6b934ec7c5b08565fb3abdbfa64aa94e24
Tyler Glisson <Tyler@tglisson.com>
2019-06-26 13:44:25 -0400

    add placeholder object for server response

--- api.js
+++ api.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
+const applicants = {
+    'firstName': 'Tyler',
+    'lastName': 'Glisson',
+    'DOB': '08.26.1984'
+};
 
 app.get('/', (req, res) =>{
     res.send('hello world');
 });
 
 app.get('/api/applicants', (req, res) => {
-    res.send([1, 2, 3]);
+    res.send(applicants);
 });
 
 app.listen(3000, () => console.log('listening on 3000')); 
