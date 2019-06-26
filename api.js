d2b71289dece03a935e2c1964010fbad7043afb1
Tyler Glisson <Tyler@tglisson.com>
2019-06-26 18:58:09 -0400

    add id query route and POST functionality

--- api.js
+++ api.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const applicants = [
     {
-        'firstName': 'Tyler',
-        'lastName': 'Glisson',
-        'DOB': '08.26.1984'
+        firstName: 'Tyler',
+        lastName: 'Glisson',
+        DOB: '08.26.1984',
+        id: 0
     },
     {
-        'firstName': 'Omar',
-        'lastName': 'Little',
-        'DOB': '01.01.1975'
+        firstName: 'Omar',
+        lastName: 'Little',
+        DOB: '01.01.1975',
+        id: 1
     },
     {
-        'firstName': 'Jimmy',
-        'lastName': 'McNulty',
-        'DOB': '01.01.1968'
+        firstName: 'Jimmy',
+        lastName: 'McNulty',
+        DOB: '01.01.1968',
+        id: 2
     },
 ];
 
@@ -24,6 +30,21 @@ app.get('/', (req, res) =>{
 
 app.get('/api/applicants', (req, res) => {
     res.send(applicants);
+});
+
+app.get('/api/applicants/:id', (req, res) => {
+    const applicant = applicants.find(app => app.id === parseInt(req.params.id));
+    if (!applicant) res.status(404).send('The applicant with the given id was not found');
+    res.send(applicant);
+});
+
+app.post('/api/applicants', (req, res) => {
+    const applicant = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName, 
+        DOB: req.body.DOB,
+        id: applicants.length + 1
+    }
 });
 
 const port = process.env.PORT || 3000;
