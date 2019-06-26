a0b23e709312942fb4235c320484cba0b4c0e38d
Tyler Glisson <Tyler@tglisson.com>
2019-06-26 13:46:41 -0400

    add 'port' environment variable instead of hard coding port as 3000

--- api.js
+++ api.js
@@ -14,4 +14,5 @@ app.get('/api/applicants', (req, res) => {
     res.send(applicants);
 });
 
-app.listen(3000, () => console.log('listening on 3000')); 
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening on ${port}`)); 
