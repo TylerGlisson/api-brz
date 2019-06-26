cd4eb5f650e83790f3c283caf23339d277f45e1e
Tyler Glisson <Tyler@tglisson.com>
2019-06-26 13:56:22 -0400

    add more applicants object

--- api.js
+++ api.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-const applicants = {
-    'firstName': 'Tyler',
-    'lastName': 'Glisson',
-    'DOB': '08.26.1984'
-};
+const applicants = [
+    {
+        'firstName': 'Tyler',
+        'lastName': 'Glisson',
+        'DOB': '08.26.1984'
+    },
+    {
+        'firstName': 'Omar',
+        'lastName': 'Little',
+        'DOB': '01.01.1975'
+    },
+    {
+        'firstName': 'Jimmy',
+        'lastName': 'McNulty',
+        'DOB': '01.01.1968'
+    },
+];
 
 app.get('/', (req, res) =>{
     res.send('hello world');
